476d2ccd89f310bbc27e49d2a555cc57121f0cd3
Marko Krstic <mailme@markokrstic.com>
2024-06-03 21:08:19 +0200

    ...

--- src/toggle-content/edit.js
+++ src/toggle-content/edit.js
@@ -4,6 +4,7 @@ import { useBlockProps, InnerBlocks, BlockControls } from '@wordpress/block-edit
 import { ToolbarGroup, ToolbarButton } from '@wordpress/components';
 import { useState } from '@wordpress/element';
 import { useSelect } from '@wordpress/data';
+import { Icon, justifyLeft, justifyCenter, justifyRight, visibility, edit } from '@wordpress/icons';
 import ToggleSwitch from './components/ToggleSwitch';
 import './editor.scss';
 
@@ -11,6 +12,7 @@ export default function Edit(props) {
 	const { clientId } = props;
 	const [isVisible, setIsVisible] = useState(true);
 	const [isEditing, setIsEditing] = useState(true);
+	const [alignment, setAlignment] = useState('left');
 
 	const toggleContent = () => {
 		setIsVisible(!isVisible);
@@ -35,16 +37,47 @@ export default function Edit(props) {
 
 	const innerClassName = isEditing ? (isVisible ? 'hide-inner-1' : 'hide-inner-2') : 'inner-in-preview';
 
+	const alignmentIcons = {
+		left: <Icon icon={justifyLeft} />,
+		center: <Icon icon={justifyCenter} />,
+		right: <Icon icon={justifyRight} />
+	};
+
 	return (
 		<div { ...useBlockProps() }>
 			<BlockControls>
 				<ToolbarGroup>
 					<ToolbarButton
-						icon={isEditing ? 'visibility' : 'edit'}
+						icon={isEditing ? visibility : edit}
 						label={isEditing ? __('Switch to Preview', 'toggle-content') : __('Switch to Edit', 'toggle-content')}
 						onClick={toggleEditing}
 					/>
 				</ToolbarGroup>
+				<ToolbarGroup
+					icon={alignmentIcons[alignment]}
+					controls={[
+						{
+							icon: <Icon icon={justifyLeft} />,
+							title: 'Left',
+							isActive: alignment === 'left',
+							onClick: () => setAlignment('left'),
+						},
+						{
+							icon: <Icon icon={justifyCenter} />,
+							title: 'Center',
+							isActive: alignment === 'center',
+							onClick: () => setAlignment('center'),
+						},
+						{
+							icon: <Icon icon={justifyRight} />,
+							title: 'Right',
+							isActive: alignment === 'right',
+							onClick: () => setAlignment('right'),
+						}
+					]}
+					isCollapsed
+					title={__('More rich text controls')}
+				/>
 			</BlockControls>
 			<ToggleSwitch isChecked={isVisible} onChange={toggleContent} />
 			<div className={innerClassName}>
